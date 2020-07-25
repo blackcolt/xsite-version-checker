bfda88ecc2de16b76b6d8264c98dc3fcf0bfb39a
Idan Magled <idanmagled@gmail.com>
2020-07-26 01:23:22 +0300

    - Some styling.  - Moved to arrow functions.  - Some code formatting.

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    (request, _, callback) => {
         if (request.contentScriptQuery === "bucket") {
             function timeout(ms, promise) {
-                return new Promise(function (resolve, reject) {
-                    setTimeout(function () {
-                        reject(new Error("timeout"))
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        reject(new Error("timeout"));
                     }, ms);
-                    promise.then(resolve, reject)
-                })
+                    promise.then(resolve, reject);
+                });
             }
 
             timeout(10000, fetch("https://ni-xbox.naturalint.com/api/bucket-bos/ui-data/pages/"))
                 .then(response => response.json())
-                .then(data => sendResponse(data))
-                .catch(error => sendResponse(error));
-            return true
+                .then(data => callback(data))
+                .catch(error => callback(error));
+            return true;
         }
     });
--- content.js
+++ content.js
@@ -1,34 +1,29 @@
 $(function () {
-    var body = $('body'), bucketName = body.attr('data-name'), bucketVersion = body.attr('data-version').trim();
+    let body = $('body'), bucketName = body.attr('data-name'), bucketVersion = body.attr('data-version');
     if (bucketName && bucketVersion) {
-        $.get(chrome.extension.getURL('/snack.html'), function (data) {
-            $(data).appendTo('body');
+        $.get(chrome.extension.getURL('/snack.html'), (htmlData) => {
+            $(htmlData).appendTo(body);
             getLatestVersion();
         });
     }
 
     function createVersionText(text, latestVersion) {
-        text = text.replace('{version}', bucketVersion)
-            .replace('{bucket-name}', bucketName);
+        text = text.replace('{version}', bucketVersion).replace('{bucket-name}', bucketName);
         if (latestVersion) {
-            return bucketVersion === latestVersion.trim() ?
-                text.replace('updated version: {new-version}', '<b>Bucket IN LATEST VERSION</b>') :
+            return bucketVersion.trim().toLowerCase() === latestVersion.trim().toLowerCase() ?
+                text.replace('updated version: {new-version}', '<b id="success"> BUCKET IN LATEST VERSION :)</b>') :
                 text.replace('{new-version}', `<b id="error">${latestVersion}</b>`);
         }
-        return text.replace(' updated version: {new-version}', '<b> Can\'t get new version :( </b>');
+        return text.replace(' updated version: {new-version}', '<b id="error"> Can\'t get new version :( </b>');
     }
 
     function getLatestVersion() {
         chrome.runtime.sendMessage(
-            {contentScriptQuery: "bucket"}, function (response) {
+            {contentScriptQuery: "bucket"}, (response) => {
                 const snack = $('#snackbar');
-                if (response.content) {
-                    snack.html(createVersionText(snack.text(), response.content[bucketName][0]));
-                } else {
-                    snack.html(createVersionText(snack.text(), false));
-                }
-                snack.addClass('show');
-                setTimeout(function () {
+                snack.html(createVersionText(snack.text(), response.content && response.content[bucketName] ? response.content[bucketName][0] : false))
+                    .addClass('show');
+                setTimeout(() => {
                     snack.removeClass('show');
                 }, 15000);
             })
